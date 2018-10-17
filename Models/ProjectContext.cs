5f662854e6079db33b6fa165ca3d0cceb557942e
Rajivrocks-Ltd <34271434+Rajivrocks-Ltd@users.noreply.github.com>
2018-10-17 02:16:44 +0200

    Changed model according to new ERD

---
+++ Models/ProjectContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projectC.Models
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options)
+        {   
+
+        }
+
+
+
+    }
+
+    public class User
+    {
+        [Key]
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+        public List<Favourite> Favourites { get; set; }
+    }
+
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; }
+    }
+
+    public class Favourite
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Product Product { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
